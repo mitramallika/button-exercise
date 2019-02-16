eb2006abdc30f6b61ccd01d161a16335fc885c31
Mallika Mitra <mallika.mitra@journalism.cuny.edu>
2019-02-16 15:15:34 -0500

    my first transfer

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$('#button-img-1').prop('disabled', true);
+	$('.cool-button').on("click", function (e) {
+		e.preventDefault();
+		$('.cool-button').prop('disabled', false);
+		$(this).prop('disabled', true);
+		var myInfo = $(this),
+			myUrl = myInfo.data('image');
+		$('#aids-img').attr('src', myURL);
+	});
+});
